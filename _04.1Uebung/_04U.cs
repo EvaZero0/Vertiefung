8b30a2a7b8e6535fd883eccba9bea9ccd50dc864
EvaZero0 <burnitdown89@hotmail.com>
2024-11-06 20:30:35 +0100

    neu

---
+++ _04.1Uebung/_04U.cs
@@ -0,0 +1,35 @@
+﻿double[] temperaturen = { 15.3, 16.2, 22.7, 30.0 };
+double totalTemp = 0;
+int warmeTage = 0;
+
+// Aufgabe: Was ist die Durchschnittstemperatur? Wieviele Tage mit Temperaturen über 20 Grad?
+for (int i = 0; i < temperaturen.Length; i++)
+{
+    totalTemp += temperaturen[i];
+
+    if (temperaturen[i] > 20)
+        warmeTage++;
+}
+Console.WriteLine("Durchschnittstemperatur: " + totalTemp / temperaturen.Length + "\nTage über 20 Grad: " + warmeTage);
+// Erstelle ein Array für Bestellpostenpreise: errechne die Gesamtsumme, ermittle den höchsten/niedrigsten Preis UND den Durchschnitt
+
+decimal[] bestPostenPreis = { 19.99m, 2.37m, 114.89m, 33.33m, 25.00m };
+decimal gesamtPreis = 0;
+decimal maxPreis = bestPostenPreis[0];
+decimal minPreis = bestPostenPreis[0];
+
+for (int i = 0; i < bestPostenPreis.Length; i++)
+{
+    gesamtPreis += bestPostenPreis[i];
+
+    if (bestPostenPreis[i] > maxPreis)
+        maxPreis = bestPostenPreis[i];
+    else if (bestPostenPreis[i] < minPreis)
+        minPreis = bestPostenPreis[i];
+}
+decimal durchschnittPreis = Math.Round(gesamtPreis / bestPostenPreis.Length, 2);
+
+Console.WriteLine($"Gesamtpreis: {gesamtPreis}" +
+    $"\nHöchster Preis: {maxPreis}" +
+    $"\nNiedrigster Preis: {minPreis}" +
+    $"\nDurchschnittspreis: {durchschnittPreis}");
--- _04Arrays_Wiederholungen/_04.cs
+++ _04Arrays_Wiederholungen/_04.cs
@@ -9,50 +9,12 @@ int letzteUngerade = ungeradeZahlen[2];
 
 // Looping-Statements
 
-//double[] temperaturen = { 15.3, 16.2, 22.7, 30.0 };
-
-//for (int i = 0; i < temperaturen.Length; i++) // i++ ist ein Inkrement, um einfach den Wert um 1 zu erhöhen
-//{
-//    Console.WriteLine(temperaturen[i]);
-//}
-
 double[] temperaturen = { 15.3, 16.2, 22.7, 30.0 };
 double totalTemp = 0;
 int warmeTage = 0;
 
-for (int i = 0; i < temperaturen.Length; i++)
+for (int i = 0; i < temperaturen.Length; i++) // i++ ist ein Inkrement, um einfach den Wert um 1 zu erhöhen
 {
     Console.WriteLine("Index von " + i + ": " + temperaturen[i]);
 }
 
-// Aufgabe: Was ist die Durchschnittstemperatur? Wieviele Tage mit Temperaturen über 20 Grad?
-for (int i = 0; i < temperaturen.Length; i++)
-{
-    totalTemp += temperaturen[i];
-
-    if (temperaturen[i] > 20)
-        warmeTage++;
-}
-Console.WriteLine("Durchschnittstemperatur: " + totalTemp / temperaturen.Length + "\nTage über 20 Grad: " + warmeTage);
-// Erstelle ein Array für Bestellpostenpreise: errechne die Gesamtsumme, ermittle den höchsten/niedrigsten Preis UND den Durchschnitt
-
-decimal[] bPostenPreis = { 19.99m, 2.37m, 114.89m, 33.33m, 25.00m };
-decimal gesamtPreis = 0;
-decimal maxPreis = bPostenPreis[0];
-decimal minPreis = bPostenPreis[0];
-
-for (int i = 0; i < bPostenPreis.Length; i++)
-{
-    gesamtPreis += bPostenPreis[i];
-
-    if (bPostenPreis[i] > maxPreis)
-        maxPreis = bPostenPreis[i];
-    else if (bPostenPreis[i] < minPreis)
-        minPreis = bPostenPreis[i];
-}
-decimal durchschnittPreis = Math.Round(gesamtPreis / bPostenPreis.Length, 2);
-
-Console.WriteLine($"Gesamtpreis: {gesamtPreis}" +
-    $"\nHöchster Preis: {maxPreis}" +
-    $"\nNiedrigster Preis: {minPreis}" +
-    $"\nDurchschnittspreis: {durchschnittPreis}");
