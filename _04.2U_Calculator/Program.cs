5afec2cda39b4fd929b3a1b67e2b683055d5e64e
EvaZero0 <168176416+EvaZero0@users.noreply.github.com>
2024-11-07 12:19:12 +0100

    added simple calculator

---
+++ _04.2U_Calculator/Program.cs
@@ -0,0 +1,50 @@
+﻿decimal zahl1, zahl2, ergebnis;
+string operation;
+
+Console.WriteLine("Calculator\n" +
+    "Anleitung:\n" +
+    "Um eine Rechnung durchzuführen, geben Sie zwei Zahlen ein und bestätigen Sie jeweils mit Enter.\n" +
+    "Wählen Sie die Rechenart und bestätigen Sie mit Enter." +
+    "Drücken Sie Q, um das Programm zu beenden.");
+
+do
+{
+    Console.WriteLine("Zahl 1:");
+    zahl1 = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Zahl 2:");
+    zahl2 = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Rechenart: Addieren (A oder +), Subtrahieren (S oder -), Multiplizieren (M oder *), Dividieren (D oder /), Beenden (Q)");
+    operation = Console.ReadLine().ToUpper();
+    switch (operation)
+    {
+        case ("A" or "+"):
+            ergebnis = zahl1 + zahl2;
+            Console.WriteLine($"{zahl1} + {zahl2} = {ergebnis}");
+            break;
+        case ("S" or "-"):
+            ergebnis = zahl1 - zahl2;
+            Console.WriteLine($"{zahl1} - {zahl2} = {ergebnis}");
+            break;
+        case ("M" or "*"):
+            ergebnis = zahl1 * zahl2;
+            Console.WriteLine($"{zahl1} * {zahl2} = {ergebnis}");
+                        break;
+            case ("D" or  "/"):
+            if (zahl2 == 0)
+            {
+                Console.WriteLine("Division durch 0 ist nicht erlaubt.");
+                break;
+            }
+            ergebnis= zahl1 / zahl2;
+            Console.WriteLine($"{zahl1} / {zahl2} = {ergebnis}");
+
+            break;
+        case ("Q"):
+            break;
+    }
+    Console.WriteLine("[Q] zum Beenden oder sonstiges drücken, zum Fortfahren.");
+    var userChoice = Console.ReadLine().ToUpper();
+    if (userChoice == "Q")
+        break;
+    
+} while (true);
