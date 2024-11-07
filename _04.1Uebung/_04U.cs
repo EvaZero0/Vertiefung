d418590f302d97374a7376a19a11a6354cb7bc39
EvaZero0 <burnitdown89@hotmail.com>
2024-11-07 09:27:41 +0100

    small changes

--- _04.1Uebung/_04U.cs
+++ _04.1Uebung/_04U.cs
@@ -1,18 +1,35 @@
 ﻿double[] temperaturen = [15.3, 16.2, 22.7, 30.0];
-double totalTemp = 0;
+double summeTemp = 0;
 int warmeTage = 0;
 
 // Aufgabe: Was ist die Durchschnittstemperatur? Wieviele Tage mit Temperaturen über 20 Grad?
 for (int i = 0; i < temperaturen.Length; i++)
 {
-    totalTemp += temperaturen[i];
+    summeTemp += temperaturen[i];
 
     if (temperaturen[i] > 20)
         warmeTage++;
 }
+var durchschnittTemp = Math.Round(summeTemp / temperaturen.Length, 2);
 
-Console.WriteLine("Durchschnittstemperatur: " + totalTemp / temperaturen.Length + " °C" +
-    "\nTage über 20 Grad: " + warmeTage);
+Console.WriteLine($"Durchschnittstemperatur: {durchschnittTemp} °C " +
+    $"\nTage über 20 Grad: {warmeTage}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Durchschnittswert eines Arrays -> temperaturen.Average()
+
 // Erstelle ein Array für Bestellpostenpreise: errechne die Gesamtsumme, ermittle den höchsten/niedrigsten Preis UND den Durchschnitt
 
 decimal[] bestPostenPreis = [19.99m, 2.37m, 114.89m, 33.33m, 25.00m];
