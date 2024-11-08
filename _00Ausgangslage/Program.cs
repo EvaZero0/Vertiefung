2b556684ccf018a518046dfe0955232945b0ac13
EvaZero0 <168176416+EvaZero0@users.noreply.github.com>
2024-11-08 10:39:15 +0100

    new (done goofed)

---
+++ _00Ausgangslage/Program.cs
@@ -0,0 +1,17 @@
+﻿// diese Programm funktioniert, ist aber nicht sehr gut lesbar (abgesehen von den Bezeichnern
+
+Console.WriteLine("erste Zahl: ");
+string ersteZahlEingabe = Console.ReadLine();
+double ersteZahl = double.Parse(ersteZahlEingabe);
+Console.WriteLine("zweite Zahl: ");
+string zweiteZahlEingabe = Console.ReadLine();
+double zweiteZahl = double.Parse(zweiteZahlEingabe);
+double ergebnis = ersteZahl + zweiteZahl;
+Console.WriteLine($"Das Ergebnis ist {ergebnis}.");
+
+// was könnte helfen?
+// - Code kommentieren - Nachteil: zu viele Kommentare, Kommentar kann obsolet werden
+
+// - Refaktorierung/Refactoring!
+// Hierbei wird der Code umgestaltet, ohne die Funktionalität zu verändern.
+// So soll der Code lesbarer werden!
--- _05Rechner/05.cs
+++ _05Rechner/05.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+bool programmLaueft = true;
+
+do
+{
+    Console.WriteLine("Wähle Operation: Addition [A], Subtraktion [S], Multiplikation [M], Division [D], Beenden [Q]");
+        string eingabe = Console.ReadLine().ToUpper();
+    char erstesZeichen = eingabe[0];
+    double zahl1 = 0, zahl2 = 0, ergebnis = 0;
+    string ausgabe = "";
+   
+
+    if (programmLaueft)
+    {
+        Console.WriteLine("Erste Zahl:");
+        zahl1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Zweite Zahl:");
+        zahl2 = double.Parse(Console.ReadLine());
+    }
+    switch (erstesZeichen)
+    {
+        case 'B' or 'Q':
+            programmLaueft = false;
+            break;
+        case 'A' or '+':
+            ergebnis = zahl1 + zahl2;
+            ausgabe = $"{zahl1} + {zahl2} = {ergebnis}";
+            break;
+    }
+    programmLaueft = false;
+} while (programmLaueft);
